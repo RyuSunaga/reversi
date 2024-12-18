261af3c19b50b4f4809732cabb5fc696c06f85eb
RyuSunaga <tetunoshin.ryu@icloud.com>
2024-12-18 09:07:03 +0900

    add player class & const values

--- src/main.lisp
+++ src/main.lisp
@@ -3,6 +3,10 @@
 ;;; 2. CLOS
 ;;; 3. マクロ
 
+;; 定数
+(defconstant +black+ #\○ "Symbol for black stone.")
+(defconstant +white+ #\● "Symbol for white stone.")
+(defconstant +space+ #\． "Symbol for white stone.")
 
 ;; Board Class
 ;; Board情報/書き換え/出力/チェック
@@ -12,19 +16,24 @@
 	 :accessor grid
 	 :documentation "Grid Infomation")))
 
-(defun initialise-grid ()
+(defun initialize-grid ()
   ;; initialize grid status.
-  (let ((grid (make-array '(8 8) :initial-element #\.)))
-    (setf (aref grid 3 3) #\o)
-    (setf (aref grid 4 4) #\o)    
-    (setf (aref grid 3 4) #\x)
-    (setf (aref grid 4 3) #\x)
+  (let ((grid (make-array '(8 8) :initial-element +space+)))
+    (setf (aref grid 3 3) +black+)
+    (setf (aref grid 4 4) +black+)    
+    (setf (aref grid 3 4) +white+)
+    (setf (aref grid 4 3) +white+)
     grid))
 
 (defgeneric display-grid (board)
   (:documentation "Display board info."))
 
 (defmethod display-grid ((board board))
+  ;; TODO：ボードを表示する際に行番号と列番号を表示できるようにしたい
+  ;; (progn
+  ;;   (format t "  ")
+  ;;   (dotimes (i 8) (format t "~A" i)
+  ;;   (format t "~%"))
   (dotimes (i 8)
     (dotimes (j 8)
       (format t "~a" (aref (grid board) i j)))
@@ -32,12 +41,31 @@
 
 ;; Player Class
 ;; Player情報/Action
-
+(defclass player ()
+  ((player-type :initarg :player-type
+		:initform (error "Must set player type.")
+		:accessor player-type
+		:documentation "Player type. Human or CPU")
+   (player-name :initarg :player-name
+		:initform (error "Must set player name.")
+		:accessor player-name)
+   (stone-type :initarg :stone-type
+	       :initform (error "Must set stone type.")
+	       :accessor stone-type
+	       :documentation "Player stone type.")))
 
 ;; Human Class -> Player Class
-
+(defclass human (player) ())
 
 ;; CPU Class -> Player Class
+(defclass cpu (player) ())
+
+;; Player & CPU sample
+(defparameter *player*
+  (make-instance 'human :player-type 'human :player-name "Ryu" :stone-type +black+))
+
+(defparameter *cpu*
+  (make-instance 'human :player-type 'cpu :player-name "MOCCA" :stone-type +white+))
 
 
 ;; User Interface
@@ -48,8 +76,6 @@
 ;;;; put
 ;;;; update -> end if game close
 ;;;; player change
-
-
 
 
 
